f8b3476131170daaf86441c4b3da3c4a8436c5df
Jani Soronen <soronen.jani@outlook.com>
2020-09-15 08:47:22 +0300

    app.js changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import './MediaQueries.css';
 
@@ -9,32 +9,36 @@ import FollowUpQuestion from './components/followUpQuestion';
 import UpdateQuestion from './components/updateQuestion';
 import DeleteQuestion from './components/deleteQuestion';
 
+import { CRUDProvider } from './components/questionContext';
+
 
 class App extends Component {
 
-  
+
 
   render() {
     var followUp = (props) => {
-      return <FollowUpQuestion allAnswerIds={props.location.allAnswerIds} newQuestionIdForFollowUp={props.location.newQuestionIdForFollowUp}/>
-      }
+      return <FollowUpQuestion allAnswerIds={props.location.allAnswerIds} newQuestionIdForFollowUp={props.location.newQuestionIdForFollowUp} />
+    }
     // var question = (props) => {
     //   return <CreateQuestion allAnswerIds={props.location.allAnswerIds}  />
     // }
     return (
-         
-          <Router>
-          <div className="App">  
-          <Route exact path="/" component={StartPage}/>  
-          <Route exact path="/createquestion" component={CreateQuestion}/> 
-          <Route exact path="/followupquestion" component={followUp}/>  
-          <Route exact path="/updatequestion" component={UpdateQuestion}/>
-          <Route exact path="/deletequestion" component={DeleteQuestion}/>
-          </div>
-        </Router>
       
+          <Router>
+            <CRUDProvider>
+            <div className="App">
+              <Route exact path="/" component={StartPage} />
+              <Route exact path="/createquestion" component={CreateQuestion} />
+              <Route exact path="/followupquestion" component={followUp} />
+              <Route exact path="/updatequestion" component={UpdateQuestion} />
+              <Route exact path="/deletequestion" component={DeleteQuestion} />
+            </div>
+            </CRUDProvider>
+          </Router>
+     
     );
   }
 }
-  
+
 export default App;
